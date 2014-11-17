4739e5a8885476d245bb82653804d52659e0d8c6
Pk <Pk@172.16.18.144>
2014-11-17 13:05:22 -0800

    skeleton

---
+++ src/Skeleton/Contact.java
@@ -0,0 +1,89 @@
+package Skeleton;
+
+import java.util.Comparator;
+
+import letsGetThisWorking.Addressbook;
+
+public class Contact {
+
+	private String firstName;
+	private String lastName;
+	private String streetAdress;
+	private String email;
+	private String phoneNumber;
+	private String notes;
+	private String zip;
+
+	
+	public Contact(String firstName, String lastName, String zip) 
+    {
+        
+    }
+    
+    public String getFirstName() 
+    {
+    	return firstName;
+    }
+    public void setFirstName(String firstName) 
+    {
+
+    }
+    //last name
+    public String getLastName() 
+    {
+    	return lastName;
+    }
+    public void setLastName(String lastName) 
+    {
+
+    }
+    public String getZip() 
+    {
+    return zip;
+    }
+    public void setZip(String zip) 
+    {
+
+    }
+   
+	public void setStreetAddress()
+	{
+		
+	}
+	public void setEmailAddress()
+	{
+		
+	}
+	public void setPhoneNumber()
+	{
+		
+	}
+	public void setNotes()
+	{
+		
+	}
+
+	//This returns a string representation of the objects.
+	public String toString()
+	{
+		return firstName;
+	}
+	
+	
+	/*Comparator for sorting the list by Student Name
+	public static Comparator<Contact> LastNameComparator = new Comparator<Contact>() 
+		{
+			public int compare(Contact ln1, Contact ln2) 
+				{
+					String LastName1 = ln1.getLastName().toLowerCase();
+					String LastName2 = ln2.getLastName().toLowerCase();
+		
+					//ascending order
+					return LastName1.compareTo(LastName2);
+		
+					//descending order
+					//return LastName2.compareTo(LastName1);
+				}
+		};*/
+
+}
---
+++ src/Skeleton/Runner.java
@@ -0,0 +1,17 @@
+package Skeleton;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		//call contact to add new contact
+
+		AddressBook addressbook = new AddressBook();
+		
+		//System.out.println("hey " + addressbook.printarray());
+				//addressbook.addContact("peter", "knight", "94040")
+		
+		//scanner goes here
+	}
+
+}
